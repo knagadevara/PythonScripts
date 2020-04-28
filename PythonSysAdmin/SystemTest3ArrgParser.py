f1e9f6f5a50fd93a00808774989894aa2ce91b2b
knagadevara <vnsk.1991@gmail.com>
2020-04-28 18:27:13 +0530

    Added exception Handeling

--- PythonSysAdmin/SystemTest3ArrgParser.py
+++ PythonSysAdmin/SystemTest3ArrgParser.py
@@ -19,7 +19,7 @@ gen1="Request you to provide the below as arguments for the file:" + \
         "\n %(prog)s ruby <name of file> \n \t for RUBY...\n"
 epilog="And that's how you'd run a the script"
 
-### Initialization of Parser
+### Initialization of Parser to collect Data
 parseme = argparse.ArgumentParser(prog='ArgumentParserTest' ,  description=argDesc, usage=gen1 , epilog=epilog )
 parseme.add_argument('--fileName', '-f', type=str, help='Give a Valid filename without any special characters', required=True)
 parseme.add_argument('--sLang', '-s', type=str, help='Give a Valid Scripting Language without any special characters')
@@ -28,14 +28,26 @@ parseme.add_argument('--fLimit', '-l', type=int, help='Give a Valid Number')
 
 ## Saving the contents into a variable
 arguments =  parseme.parse_args()
-print(type(arguments))
 
-with open(arguments.fileName, 'r') as rf1:
-    readln = rf1.readlines()
-    readln.reverse()
-
-    if arguments.fLimit:
-        readln = readln[:arguments.fLimit]
-
-    for line in readln:
-        print(line.strip()[::-1])
+## Logic of the program
+try:
+    rf1 = open(arguments.fileName, 'r')
+except PermissionError:
+    print('Do not have Permission to access')
+except FileNotFoundError:
+    print('Unable to read, NO File Found')
+else:
+    with rf1:
+        try:
+            readln = rf1.readlines()
+            readln.reverse()
+        except UnicodeDecodeError:
+            print('Unable to read, NOT a text file')
+        else:
+            if arguments.fLimit:
+                readln = readln[:arguments.fLimit]
+            for line in readln:
+                print(line.strip()[::-1])
+finally:
+    ### Originally used to 'close' on the opened connections.
+    print('\n Completed Running!!')
