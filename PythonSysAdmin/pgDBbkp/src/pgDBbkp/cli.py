ce0e87fc51adccc33aba006d6137d532380fd0dc
knagadevara <vnsk.1991@gmail.com>
2020-05-09 17:45:36 +0530

    Identify and restrict given  driver type

--- PythonSysAdmin/pgDBbkp/src/pgDBbkp/cli.py
+++ PythonSysAdmin/pgDBbkp/src/pgDBbkp/cli.py
@@ -11,18 +11,21 @@
 
 import argparse
 
+parse_drivers = ['local' , 'remote' , 's3']
 class DriverAction(argparse.Action):
     def __call__(self, parser, namespace, values, option_string=None):
         driver, destination = values
-        namespace.driver = driver.lower()
-        namespace.destination = destination
-        return super().__call__(parser, namespace, values)
-
+        if driver.lower() in parse_drivers:
+            parser.error("Unknown driver given")
+        else:
+            namespace.driver = driver.lower()
+            namespace.destination = destination
+            return super().__call__(parser, namespace, values)
 
 def create_parser():
     parser = argparse.ArgumentParser(prog='pgDBbkp', description=""" Backup PostgreSQL DB locally or to AWS S3 """)
-    parser.add_argument('--url', '-u' , help='URL for the DB to backup')
-    parser.add_argument('--driver', '-d' ,
+    parser.add_argument('--url', '-u' , help='URL for the DB to backup' , type= str)
+    parser.add_argument('--driver', '-d' , type= str ,
     help='type of driver and destination should be provided' , nargs=2,
     action=DriverAction, required=True)
-    return parser
+    return
