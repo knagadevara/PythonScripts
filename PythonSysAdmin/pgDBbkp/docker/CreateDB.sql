9e05838866475597edd36c83fd438fa4bce2cf40
knagadevara <vnsk.1991@gmail.com>
2020-05-09 07:50:27 +0530

    Create company2020 DB as a feed to script

---
+++ PythonSysAdmin/pgDBbkp/docker/CreateDB.sql
@@ -0,0 +1,9 @@
+DROP DATABASE "company_2020";
+CREATE DATABASE "company_2020"
+     WITH OWNER = "knagadevara"
+	    TEMPLATE = "template0"
+            ENCODING = "UTF8"
+            LC_COLLATE = "English_United States.1252"
+            LC_CTYPE = "English_United States.1252"
+            TABLESPACE = pg_default
+            CONNECTION LIMIT = -1;
