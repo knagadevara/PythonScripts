084fcb27951721044f2aa9a204139d8a76f6bc95
knagadevara <vnsk.1991@gmail.com>
2020-05-09 08:01:33 +0530

    pgDBbkp package

---
+++ PythonSysAdmin/pgDBbkp/src/pgDBbkp/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+#--- --- --- --- --- --- --- --- --- ---##--- --- --- --- --- --- --- --- --- ---#
+
+        # By Author: knagadevara
+        # Date: Sat May  9 07:28:27 IST 2020
+        # Scripting Language: python
+        # Copyright:: 2020, The Authors, All Rights Reserved.
+
+#--- --- --- --- --- --- --- --- --- ---##--- --- --- --- --- --- --- --- --- ---#
