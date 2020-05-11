9ec9c8ef5a448c0ed841d58e4a22a065c7bf833c
knagadevara <vnsk.1991@gmail.com>
2020-05-12 02:27:42 +0530

    added a statement to use the newly created Application profiles_api functionality

--- WebApplication/Django/profiles_project/settings.py
+++ WebApplication/Django/profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+## Added the below to use the custom created UserProfile model
+AUTH_USER_MODEL = 'profiles_api.UserProfiles'
