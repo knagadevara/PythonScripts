a9e0c686bb351ba9b57a39b5a6438b32b2f04d96
knagadevara <vnsk.1991@gmail.com>
2020-05-12 19:04:17 +0530

    Added UserProfiles method to be used as a default for user model.

--- WebApplication/Django/UserAuthApp/profiles_api/admin.py
+++ WebApplication/Django/UserAuthApp/profiles_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+## Adding the userdefined model to admin
+from profiles_api import models
 # Register your models here.
+
+admin.site.register(models.UserProfiles)
