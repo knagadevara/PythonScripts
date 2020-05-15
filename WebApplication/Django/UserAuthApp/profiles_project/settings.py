bcdb441a786c425ccf3ecd7a8e206cf904e7fb81
knagadevara <vnsk.1991@gmail.com>
2020-05-15 23:48:56 +0530

    Added and enabled the functionality of the application class UserProfile

--- WebApplication/Django/UserAuthApp/profiles_project/settings.py
+++ WebApplication/Django/UserAuthApp/profiles_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'profiles_api',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
