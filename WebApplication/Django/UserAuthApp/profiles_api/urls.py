ff3ab0b4cc378394d6dd51393742cb5f18e14671
knagadevara <vnsk.1991@gmail.com>
2020-05-21 20:19:25 +0530

    added Viewset

--- WebApplication/Django/UserAuthApp/profiles_api/urls.py
+++ WebApplication/Django/UserAuthApp/profiles_api/urls.py
@@ -1,8 +1,13 @@
 ## The requests that begin with "api/" as a prefix will be responded from here 'profiles_api.urls'
 ## This file contains all the suffixe/sublink forwards
-from django.urls import path
+from django.urls import path , include
+from rest_framework.routers import DefaultRouter 
 from profiles_api import views
 
+router = DefaultRouter()
+router.register('hello_viewset' , views.HelloViewSet , basename='hello_viewset')
+
 urlpatterns = [
-    path('hello_api/' , views.HelloAPIView.as_view()),  
+    path('hello_api/' , views.HelloAPIView.as_view()),
+    path('' , include(router.urls))
 ]
