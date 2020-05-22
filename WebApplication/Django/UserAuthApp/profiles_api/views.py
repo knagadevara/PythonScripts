9a6423e6a6c4c7e6babd8bb365c1000c5ccfb3b7
knagadevara <vnsk.1991@gmail.com>
2020-05-23 02:05:25 +0530

    created UserProfilesViewSet

--- WebApplication/Django/UserAuthApp/profiles_api/views.py
+++ WebApplication/Django/UserAuthApp/profiles_api/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status , viewsets
-from profiles_api import serializers
+from profiles_api import serializers , models
 
 class HelloAPIView(APIView):
     """ Test api View, application logic for the endpoint will be defined """
@@ -85,3 +85,9 @@ class HelloViewSet(viewsets.ViewSet):
     
     def destroy(self, request, pk=None):
         return Response({'http_method':'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handle creating and updating profiles """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+
