d5c6e176307c160f685d65245307e9db8630c5a3
knagadevara <vnsk.1991@gmail.com>
2020-05-23 12:34:58 +0530

    Created  ProfileFeedItem serialiser to persist and ciobtain the data

--- WebApplication/Django/UserAuthApp/profiles_api/serializers.py
+++ WebApplication/Django/UserAuthApp/profiles_api/serializers.py
@@ -40,3 +40,10 @@ class UserProfileSerializer(serializers.ModelSerializer):
         )
 
         return user
+
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    """ Takes in data and  """
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id' , 'user_profile' , 'status_text' , 'created_on' )
+        extra_kwargs = {'user_profile' : {'read_only' : True}}
