8c446c4bf5256bf02289a322f4b5d9558a53170c
knagadevara <vnsk.1991@gmail.com>
2020-05-23 09:36:01 +0530

    Evaluates if the id is same as the id of row where the firelds are changing

---
+++ WebApplication/Django/UserAuthApp/profiles_api/permissions.py
@@ -0,0 +1,16 @@
+from rest_framework import permissions
+
+## Making a custom permission class to restrict users from editing the details of other users
+class UpdateOwnProfile(permissions.BasePermission):
+    """ Allow users to edit their own profile """
+
+    ## Method gets called every time a request is made to the api ths permission class is assigned to
+    ## It will return a boolean value [True or False] basing on whether user/entity have correct permissions 
+    ## on the fields in the table they are trying to change  
+    def has_object_permission(self, request, view, obj):
+        """ Check if user is trying to edit their own profile """
+        ## Adding the checks below to see if the request is in SAFE_METHOD i.e GET
+        if request.method in permissions.SAFE_METHODS:
+            return True      
+        ## Evaluates if the id is same as the id of row where the firelds are changing
+        return obj.id == request.user.id
