9b557a8e3ab4c45bc0199cd47d4431843e6dec65
knagadevara <vnsk.1991@gmail.com>
2021-01-12 01:10:52 +0530

    Removing file after displaying

--- SysAdmin/checkConnect.sh
+++ SysAdmin/checkConnect.sh
@@ -8,36 +8,30 @@ UsageDialog="This tool will only take single parameter, please find the below US
 
 
 if [[ ${#} -ne 1 ]]
-then
-echo -e ${UsageDialog}
-exit 1
+	then
+	echo -e ${UsageDialog}
+	exit 1
 fi
 
 if [[ ! -f "${myPATH}/${fileLOC}" ]]
-then
-echo "File not there"
-exit 2
-else
-touch ${OutFile}
-fi
+	then
+		echo "File not there"
+		exit 2
+	else
+		touch ${OutFile}
+	fi
 
 for NewHosts in $( cat "${myPATH}/${fileLOC}" )
-
-do
-
-${PingHOST} ${NewHosts} &>/dev/null
-
-PingStatus=`echo $?`
-
-echo $PingStatus
-
-if [ $PingStatus -eq 0 ]
-
-then
-echo "${NewHosts} Network-PING SUCCESS" >> ${OutFile}
-else
-echo "${NewHosts} Network-PING FAILED" >> ${OutFile}
-
-fi
-
+	do
+	${PingHOST} ${NewHosts} &>/dev/null
+	PingStatus=`echo $?`
+	if [[ $PingStatus -eq 0 ]]
+		then
+			echo "${NewHosts} Network-PING SUCCESS" >> ${OutFile}
+		else
+			echo "${NewHosts} Network-PING FAILED" >> ${OutFile}
+	fi
 done
+
+cat $OutFile
+rm -rf $OutFile
